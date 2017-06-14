8afd8de6b1d3cfd078cb337aee6d8cf8e246f0b7
Mateusz Zając <zajac.mateusz@icloud.com>
2017-06-14 15:18:17 +0200

    Mre updates related to Swift 4

--- Package.swift
+++ Package.swift
@@ -4,15 +4,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "CodeRunner",
+    name: "CodeRunnerCLI",
+    products: [
+        .executable(name: "coderunner", targets: ["CodeRunner"])
+    ],
     dependencies: [
         .package(url: "https://github.com/JohnSundell/Files", from: "1.9.0"),
         .package(url: "https://github.com/JohnSundell/ShellOut", from: "1.0.0"),
         .package(url: "https://github.com/JohnSundell/Require", from: "1.0.0")
     ],
     targets: [
+        .target(name: "CodeRunner", dependencies: ["CodeRunnerCore"]),
         .target(name: "CodeRunnerCore", dependencies: ["Files", "ShellOut", "Require"]),
         .testTarget(name: "CodeRunnerTests", dependencies: ["CodeRunnerCore"])
     ],
-    swiftLanguageVersions: [3, 4]
+    swiftLanguageVersions: [4]
 )
