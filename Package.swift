251dd3caa5d93f5ae3ae3eb57d309e1509e74778
Mateusz Zając <zajac.mateusz@icloud.com>
2017-06-14 15:28:13 +0200

    Cleanup in Packages

--- Package.swift
+++ Package.swift
@@ -5,18 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "CodeRunnerCLI",
-    products: [
-        .executable(name: "coderunner", targets: ["CodeRunner"])
-    ],
     dependencies: [
         .package(url: "https://github.com/JohnSundell/Files", from: "1.9.0"),
         .package(url: "https://github.com/JohnSundell/ShellOut", from: "1.0.0"),
         .package(url: "https://github.com/JohnSundell/Require", from: "1.0.0")
     ],
     targets: [
-        .target(name: "CodeRunner", dependencies: ["CodeRunnerCore"]),
-        .target(name: "CodeRunnerCore", dependencies: ["Files", "ShellOut", "Require"]),
-        .testTarget(name: "CodeRunnerTests", dependencies: ["CodeRunnerCore"])
+        .target(name: "CodeRunnerCLI", dependencies: ["CodeRunnerCore"]),
+        .testTarget(name: "CodeRunnerCLITests", dependencies: ["CodeRunnerCore"]),
+        .target(name: "CodeRunnerCore", dependencies: ["Files", "ShellOut", "Require"])
     ],
     swiftLanguageVersions: [4]
 )
